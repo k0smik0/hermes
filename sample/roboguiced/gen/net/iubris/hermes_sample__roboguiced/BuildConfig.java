680d5e045076c43aff83dc147c20ddc8b9d58c6e
Massimiliano Leone <maximilianus@gmail.com>
2013-06-20 18:18:52 +0200

    untracked bin/*

--- sample/roboguiced/gen/net/iubris/hermes_sample__roboguiced/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package net.iubris.hermes_sample__roboguiced;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- sample/vanilla/gen/net/iubris/hermes_vanilla_sample/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package net.iubris.hermes_vanilla_sample;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
