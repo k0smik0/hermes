b42957e377480356b71d4c5bec28f0be2f6a4353
Massimiliano Leone <maximilianus@gmail.com>
2013-07-04 16:40:43 +0200

    added license: lgpl3 to libraries, gpl3 to sample demo app

--- sample/roboguiced/test/net/iubris/ex/test/InjectedTestRunner.java
+++ sample/roboguiced/test/net/iubris/ex/test/InjectedTestRunner.java
@@ -1,20 +1,20 @@
 /*******************************************************************************
- * Copyleft 2012 Massimiliano Leone - massimiliano.leone@iubris.net .
+ * Copyleft 2013 Massimiliano Leone - massimiliano.leone@iubris.net .
  * 
- * InjectedTestRunner.java is part of socrates.
+ * InjectedTestRunner.java is part of 'Hermes'.
  * 
- * socrates is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * 'Hermes' is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  * 
- * socrates is distributed in the hope that it will be useful,
+ * 'Hermes' is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with socrates ; if not, write to the Free Software
+ * You should have received a copy of the GNU General Public License
+ * along with 'Hermes'; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  ******************************************************************************/
 package net.iubris.ex.test;
